120d7d5643fb0c64eba5b2c18abba14161054823
Corey Powell <mistdragon100@gmail.com>
2016-01-19 10:55:15 -0500

    Added some more docs and code style fixes

--- lib/bit_table/bit_table.rb
+++ lib/bit_table/bit_table.rb
@@ -1,12 +1,21 @@
+# A BitTable as its name implies stores BITS, not Bytes, BITS
+# The assumed usage is for a large table of `flags`, such as user ids
+# Each bit could mark whether the user "exists" or something of that fashion
 class BitTable
+  # Its a Version module!
   module Version
     MAJOR, MINOR, TEENY, PATCH = 1, 0, 0, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
   end
+  # @return [String] version of BitTable
   VERSION = Version::STRING
 
+  # @attribute [r] target
+  #   @return [#seek, #tell, #write, #rewind, #readbyte] target
   attr_reader :target
 
+  # Initializes the BitTable with the given stream
+  #
   # @param [#seek, #tell, #write, #rewind, #readbyte] target
   # @param [Hash<Symbol, Object>] options
   #   @option :options [Boolean] auto_resize
@@ -24,20 +33,26 @@ class BitTable
     @target.tell
   end
 
+  # @param [Integer] index
+  # @return [Array[block_index, sub_index]]
   private def calc_bit_index(index)
     block_index = (index / 8)
     sub_index = index % 8
     return block_index, sub_index
   end
 
+  # @param [Integer] index
+  # @return [Array[block_index, sub_index]]
   private def seek_bit_index(index)
     b, s = calc_bit_index index
     @target.seek b, IO::SEEK_SET
     return b, s
   end
 
+  # @param [Integer] index
+  # @return [Boolean] true, if the given index is in bounds, false otherwise
   private def in_bounds?(index)
-    b, s = calc_bit_index index
+    b, _ = calc_bit_index index
     b < stream_size
   end
 
@@ -52,7 +67,7 @@ class BitTable
 
   private def check_resize(index)
     if @auto_resize
-      b, s = calc_bit_index index
+      b, _ = calc_bit_index index
       @target.write("\0") until b < stream_size
       @target.rewind
     else
@@ -84,6 +99,9 @@ class BitTable
     stream_size
   end
 
+  # Retrieves value at the given index
+  #   if :auto_resize was specified at initialization, then this method
+  #   will raise an IndexError if the index is out of bounds.
   # @param [Integer] index
   # @return [Integer] value
   def [](index)
@@ -91,17 +109,22 @@ class BitTable
     _, s = seek_bit_index index
     (@target.readbyte >> s) & 1
   end
+  alias :get :[]
 
+  # Sets a `value` at the given `index`
+  #   if :auto_resize was specified at initialization, then this method
+  #   will raise an IndexError if the index is out of bounds.
   # @param [Integer] index
   # @param [Boolean, Integer, nil] value
   def []=(index, value)
     check_resize index
     seek_bit_index index
     byte = @target.readbyte
-    b, s = seek_bit_index index
+    _, s = seek_bit_index index
     bit = 1 << s
     mask = 0xF ^ bit
     result = (byte & mask) | (cast_bit(value) << s)
     @target.write result.chr
   end
+  alias :put :[]=
 end
